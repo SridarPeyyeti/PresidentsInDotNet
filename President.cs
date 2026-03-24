dde5b694060abab8e1df974bff29ab8edd4706c1
SridarPeyyeti <43823203+SridarPeyyeti@users.noreply.github.com>
2026-03-24 12:46:32 -0500

    Add President model with properties
    Model

---
+++ President.cs
@@ -0,0 +1,11 @@
+namespace PresidentsApp.Models
+{
+    public class President
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Years { get; set; }
+        public string Hobby { get; set; }
+    }
+}
